e33571226332fb07dab34b1459ce35570ad2db51
Christophe Elavumoottil <krisparisgit@gmail.com>
2014-06-03 20:33:31 +0200

    <feat> Added the Universe class with the definition of values of celerity and the strength of gravity.

---
+++ project/game3d/src/main/java/com/krisparis/game3d/model/Universe.java
@@ -0,0 +1,29 @@
+package com.krisparis.game3d.model;
+
+/**
+ * <p>
+ * The universe which defines the physical laws.
+ * It is assumed that there is only one universe.
+ * </p>
+ */
+public class Universe {
+
+	
+	/**
+	 * <p>
+	 * The value of the speed of light in meter per second as defined
+	 * in US-1.
+	 * </p>
+	 */
+	public static final int celerity = 299792458;	
+	
+	/**
+	 * <p>
+	 * The value of the strength of gravity in m/(s^2) as defined
+	 * in US-1.
+	 * </p>
+	 */
+	public static final float gravity = 9.81f;
+	
+
+}
